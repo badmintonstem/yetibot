15f62905f9b03062c3d60436f2aee36ab43dd90e
badmintonstem <55669583+badmintonstem@users.noreply.github.com>
2021-12-01 12:15:55 +0000

    Upgraded to python3

--- yeti2linefollower.py
+++ yeti2linefollower.py
@@ -31,13 +31,13 @@ ZB.Init()
 if not ZB.foundChip:
     boards = ZeroBorg.ScanForZeroBorg()
     if len(boards) == 0:
-        print 'No ZeroBorg found, check you are attached :)'
+        print('No ZeroBorg found, check you are attached :)')
     else:
-        print 'No ZeroBorg at address %02X, but we did find boards:' % (ZB.i2cAddress)
+        print('No ZeroBorg at address %02X, but we did find boards:' % (ZB.i2cAddress))
         for board in boards:
-            print '    %02X (%d)' % (board, board)
-        print 'If you need to change the I²C address change the setup line so it is correct, e.g.'
-        print 'ZB.i2cAddress = 0x%02X' % (boards[0])
+            print('    %02X (%d)' % (board, board))
+        print('If you need to change the I²C address change the setup line so it is correct, e.g.')
+        print('ZB.i2cAddress = 0x%02X' % (boards[0]))
     sys.exit()
 #ZB.SetEpoIgnore(True)                 # Uncomment to disable EPO latch, needed if you do not have a switch / jumper
 ZB.SetCommsFailsafe(False)             # Disable the communications failsafe
